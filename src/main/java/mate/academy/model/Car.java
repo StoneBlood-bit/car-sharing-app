3166ead52a23a9bb3853ccc834fcad1c26275aaa
StoneBlood <brkalix@gmail.com>
2025-01-15 15:13:02 +0200

    Added checkstyle

---
+++ src/main/java/mate/academy/model/Car.java
@@ -0,0 +1,14 @@
+package mate.academy.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "cars")
+@Getter
+@Setter
+
+public class Car {
+}
