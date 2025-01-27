0220f6478226c1896d85e1f724ddb96baf70f1a4
StoneBlood <brkalix@gmail.com>
2025-01-27 13:30:24 +0200

    added fetch Lazy to fields `car` and `user` in entity `Rental` and to field `rental` in entity `Payment`

--- src/main/java/mate/academy/model/Payment.java
+++ src/main/java/mate/academy/model/Payment.java
@@ -1,16 +1,17 @@
 package mate.academy.model;
 
+import java.math.BigDecimal;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-import java.math.BigDecimal;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.validator.constraints.URL;
@@ -32,7 +33,7 @@ public class Payment {
     @Column(nullable = false)
     private Type type;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "rental_id", nullable = false)
     private Rental rental;
 
--- src/main/java/mate/academy/model/Rental.java
+++ src/main/java/mate/academy/model/Rental.java
@@ -1,14 +1,15 @@
 package mate.academy.model;
 
+import java.time.LocalDateTime;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-import java.time.LocalDateTime;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -30,11 +31,11 @@ public class Rental {
     @Column(name = "actual_return_date")
     private LocalDateTime actualReturnDate;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "car_id", nullable = false)
     private Car car;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 }
